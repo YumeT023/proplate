e35c3b5b9056c9928e11f5e3258d6543e532a9a8
Yume <hei.hajatiana@gmail.com>
2023-12-04 20:49:31 +0300

    refactor(core): template resolver

--- crates/core/src/template/resolver.rs
+++ crates/core/src/template/resolver.rs
@@ -10,38 +10,34 @@ use proplate_integration::git;
 
 use super::types::Template;
 
-const BUILT_IN_TEMPLATE_DIR: &str = "built_in";
-
 pub fn find_template(location: &str) -> ProplateResult<Template> {
-    let is_remote = is_remote_loc(location);
-    match is_remote {
-        true => try_clone_git_template(location),
-        false => find_builtin_template_by_id(location),
+    match is_remote_loc(location) {
+        true => clone_git_template(location),
+        false => get_local_template(location),
     }
 }
 
-fn find_builtin_template_by_id(id: &str) -> ProplateResult<Template> {
-    let path = get_template_path_by_id(id);
+fn get_local_template(dir: &str) -> ProplateResult<Template> {
+    let path = Path::new(dir);
     if !path.exists() {
-        return Err(ProplateError::local_template_not_found(id));
+        return Err(ProplateError::local_template_not_found(dir));
     }
-    explore_meta(path.try_into().unwrap(), &id, None)
-        .map_err(|_| ProplateError::local_template_not_found(id))
+    explore_meta(path.try_into().unwrap(), &dir, None)
+        .map_err(|_| ProplateError::local_template_not_found(dir))
 }
 
-fn try_clone_git_template(url: &str) -> ProplateResult<Template> {
+fn clone_git_template(url: &str) -> ProplateResult<Template> {
     let path = url.strip_prefix("https://github.com/").unwrap();
     let id = path.split("/").collect::<Vec<_>>().join("-");
     let path = format!(".temp/{}-{}", id, Uuid::new_v4());
     git::exec_cmd(["clone", url, &path], &current_dir().unwrap())
         .map_err(|_| ProplateError::remote_template_not_found(url))?;
     explore_meta(path.try_into().unwrap(), &id, Some(url.to_string()))
-        .map_err(|_| ProplateError::remote_template_not_found(url))
+        .map_err(|e| ProplateError::fs(&e.to_string()))
 }
 
 fn explore_meta(path: PathBuf, id: &str, source: Option<String>) -> Result<Template, Error> {
-    let entries = read_dir(&path)?;
-    let file_list = entries
+    let file_list = read_dir(&path)?
         .into_iter()
         .filter_map(|e| match e {
             Ok(entry) => Some(entry.file_name()),
@@ -53,8 +49,4 @@ fn explore_meta(path: PathBuf, id: &str, source: Option<String>) -> Result<Templ
 
 fn is_remote_loc(location: &str) -> bool {
     location.starts_with("https://github.com/")
-}
-
-fn get_template_path_by_id(id: &str) -> PathBuf {
-    Path::new(BUILT_IN_TEMPLATE_DIR).join(Path::new(id))
 }
