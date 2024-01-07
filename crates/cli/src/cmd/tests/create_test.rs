52d9d2ed756a65ada72ac785f5256d957b158b5f
Yume <hei.hajatiana@gmail.com>
2024-01-07 12:06:04 +0300

    test: conditional_ops

--- crates/cli/src/cmd/tests/create_test.rs
+++ crates/cli/src/cmd/tests/create_test.rs
@@ -100,11 +100,90 @@ mod dynamic_files {
 
         Ok(())
       },
-      /*clean*/ true,
+      /*clean*/ false,
     );
   }
 }
 
 #[cfg(test)]
 mod additional_ops {
+  use crate::cmd::create::{create, CreateOptions};
+  use crate::cmd::tests::{get_sample, new_trash, run_isolated_test};
+  use crate::{assert_gen_ok, assert_gen_snapshot, test_create};
+
+  use std::collections::HashMap;
+
+  use proplate_core::{
+    fs::is_dir_superset,
+    template::{resolver::clone_template, META_CONF},
+  };
+
+  #[test]
+  fn unlicensed() {
+    run_isolated_test(
+      || {
+        let ctx = HashMap::from([
+          ("project_name".to_string(), "unlicensed".to_string()),
+          ("author_name".to_string(), "Proplate".to_string()),
+          ("license".to_string(), "UNLICENSED".to_string()),
+        ]);
+
+        let (path, _) = test_create!("additional_ops", "conditional-license", ctx);
+        // we got custom snap path here
+        let (snap, _) = get_sample("additional_ops", "unlicensed-snapshot");
+
+        assert_gen_ok!(&path);
+        assert_gen_snapshot!(&snap, &path);
+
+        Ok(())
+      },
+      /*clean*/ false,
+    );
+  }
+
+  #[test]
+  fn bsd_2_clause() {
+    run_isolated_test(
+      || {
+        let ctx = HashMap::from([
+          ("project_name".to_string(), "bsd-2-clause".to_string()),
+          ("author_name".to_string(), "Proplate".to_string()),
+          ("license".to_string(), "BSD-2-Clause".to_string()),
+        ]);
+
+        let (path, _) = test_create!("additional_ops", "conditional-license", ctx);
+        // we got custom snap path here
+        let (snap, _) = get_sample("additional_ops", "bsd-2-clause-license-snapshot");
+
+        assert_gen_ok!(&path);
+        assert_gen_snapshot!(&snap, &path);
+
+        Ok(())
+      },
+      /*clean*/ false,
+    );
+  }
+
+  #[test]
+  fn mit() {
+    run_isolated_test(
+      || {
+        let ctx = HashMap::from([
+          ("project_name".to_string(), "mit".to_string()),
+          ("author_name".to_string(), "Proplate".to_string()),
+          ("license".to_string(), "MIT".to_string()),
+        ]);
+
+        let (path, _) = test_create!("additional_ops", "conditional-license", ctx);
+        // we got custom snap path here
+        let (snap, _) = get_sample("additional_ops", "mit-license-snapshot");
+
+        assert_gen_ok!(&path);
+        assert_gen_snapshot!(&snap, &path);
+
+        Ok(())
+      },
+      /*clean*/ true,
+    );
+  }
 }
