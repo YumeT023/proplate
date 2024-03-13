be84f3d0eea5d1d527fd6202fa00edcd798cf351
Yume <hei.hajatiana@gmail.com>
2024-03-13 19:53:48 +0300

    fix: typo

--- crates/cli/src/cmd/init.rs
+++ crates/cli/src/cmd/init.rs
@@ -9,7 +9,7 @@ use proplate_tui::logger;
 const INIT_TEMPLATE_GIT_REPO: &str = "https://github.com/YumeT023/tiniest-proplate";
 
 const CANNOT_INIT_LOCALLY_WARNING_MSG: &str = r#"Unable to find proplate 'builtins' dir so we're going to init your template through git repo instead.
-To remove this warning, either download proplate from gha release so it comes with the 'builtins' dir automatically or manually get the 'builtins' dir from proplate repo https://github.com/YumeT023/proplate."#;
+To remove this warning, download builtins.zip directory from https://github.com/YumeT023/proplate/releases and extract it in the same dir as proplate executable"#;
 
 pub fn init(id: String, dest: Option<String>) -> ProplateResult<String> {
   let dest = dest.unwrap_or(id.clone());
